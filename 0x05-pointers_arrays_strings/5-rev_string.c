9eef88214ac775957ad3dc7f58fe14b439cd6afc
Friday Peace <peacefriday709@gmail.com>
2023-06-26 17:07:12 -0700

    My pointers_arrays_strings task

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * rev_string - reverses a string
- * @s: input string
- *Return: String in reverse
+ * rev_string - Reverses a string
+ * @s: Input string
+ * Return: String in reverse
  */
 
 void rev_string(char *s)
@@ -12,8 +12,8 @@ void rev_string(char *s)
 	int counter = 0;
 	int i;
 
-	while (s[counter] != '\n')
-		counter++;
+	while (s[counter] != '\0')
+	counter++;
 	for (i = 0; i < counter; i++)
 	{
 		counter--;
